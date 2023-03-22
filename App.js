316cd3819f4c5c2fa4afed9c34d9e6f61c2709ab
PureHorizon <96225455+PureHorizon@users.noreply.github.com>
2023-03-22 09:14:11 +0100

    Added Map and Position Button

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    region: null,
+  };
+
+  onRegionChange = region => {
+    this.setState({ region });
+  };
+
+  onMyLocationPress = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const region = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        };
+        this.setState({ region });
+      },
+      error => console.log(error),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={this.state.region}
+          onRegionChange={this.onRegionChange}
+        >
+          {this.state.region && (
+            <Marker coordinate={this.state.region} />
+          )}
+        </MapView>
+        <TouchableOpacity
+          style={styles.myLocationButton}
+          onPress={this.onMyLocationPress}
+        >
+          <MaterialIcons name="my-location" size={24} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  myLocationButton: {
+    backgroundColor: '#009688',
+    borderRadius: 100,
+    width: 56,
+    height: 56,
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    elevation: 5,
   },
 });
