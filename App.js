2176a0324d556a329fa00d3321f4509a5060498e
PureHorizon <96225455+PureHorizon@users.noreply.github.com>
2023-03-23 18:28:03 +0100

    OpenStreetMap Map delete buttons

--- App.js
+++ App.js
@@ -3,16 +3,17 @@ import { WebView } from 'react-native-webview';
 import { View } from 'react-native';
 
 class App extends Component {
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <WebView
-          source={{ uri: 'https://www.openstreetmap.org' }}
-          style={{flex: 1}}
-        />
-      </View>
-    );
-  }
+render() {
+return (
+<View style={{flex: 1}}>
+<WebView
+source={{ uri: 'https://www.openstreetmap.org/export/embed.html?bbox=-180,-90,180,90&layer=mapnik&marker=' }}
+style={{flex: 1}}
+scalesPageToFit={false}
+/>
+</View>
+);
+}
 }
 
 export default App;
