be4b3f3d9aeac77e4d80ad311556454d6ffdf8a5
PureHorizon <96225455+PureHorizon@users.noreply.github.com>
2023-03-23 18:24:53 +0100

    OpenStreetMap Map

--- App.js
+++ App.js
@@ -1,75 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { WebView } from 'react-native-webview';
+import { View } from 'react-native';
 
-export default class App extends Component {
-  state = {
-    region: null,
-  };
-
-  onRegionChange = region => {
-    this.setState({ region });
-  };
-
-  onMyLocationPress = () => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const region = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        };
-        this.setState({ region });
-      },
-      error => console.log(error),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
-  };
-
+class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={this.state.region}
-          onRegionChange={this.onRegionChange}
-        >
-          {this.state.region && (
-            <Marker coordinate={this.state.region} />
-          )}
-        </MapView>
-        <TouchableOpacity
-          style={styles.myLocationButton}
-          onPress={this.onMyLocationPress}
-        >
-          <MaterialIcons name="my-location" size={24} color="#fff" />
-        </TouchableOpacity>
+      <View style={{flex: 1}}>
+        <WebView
+          source={{ uri: 'https://www.openstreetmap.org' }}
+          style={{flex: 1}}
+        />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  myLocationButton: {
-    backgroundColor: '#009688',
-    borderRadius: 100,
-    width: 56,
-    height: 56,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    bottom: 16,
-    right: 16,
-    elevation: 5,
-  },
-});
+export default App;
